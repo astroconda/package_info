746085a179855906503ef3d2ceb425dc6b0ffa27
Joseph Hunkeler <jhunkeler@gmail.com>
2020-09-27 18:58:43 -0400

    Add __version__

--- package_info/__init__.py
+++ package_info/__init__.py
@@ -1,1 +1,10 @@
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'unknown'
+
 from . import package_data
